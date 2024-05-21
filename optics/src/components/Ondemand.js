49180766d63f19e9544a00507a75beda2275e253
abk9 <akhila.bk2@dxc.com>
2024-05-21 08:32:17 +0530

    Changes to the positioning of the on-demand cost graph

--- optics/src/components/Ondemand.js
+++ optics/src/components/Ondemand.js
@@ -130,7 +130,7 @@ const Ondemand = () => {
         Anomalies Detection for On-Demand Cost
       </Typography>
 
-      <div className="chart" style={{ width: "500px", height: "300px" }}>
+      <div className="chart" style={{ width: "500px", height: "320px", marginBottom : "10px", marginTop: "-20px" }}>
         <canvas ref={chartContainer} />
       </div>
     </div>
