b80e15792eb3eb416eb893d5731a315df33ea7fc
Dipa Sah <dipa.sah@dxc.com>
2024-06-28 18:25:03 +0530

    updated for keycloak redirection

--- optics/src/App.js
+++ optics/src/App.js
@@ -1,5 +1,49 @@
-import React, { useEffect } from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// import React, { useEffect } from "react";
+// import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// import Home from "./components/Home";
+// import Signup from "./components/Signup";
+// import Login from "./components/Login";
+// import Dashboard from "./components/Dashboard";
+// import BillOverview from "./components/BillOverview";
+// import BusinessCostSplit from "./components/BusinessCostSplit";
+// import InventoryCostSplit from "./components/InventoryCostSplit";
+// import OrphanedSnapshots from "./components/OrphanedSnapshots";
+// import UnattachedManagedDisks from "./components/UnattachedManagedDisks";
+// import { useAuth } from "./hooks/useAuth";
+
+// import "./App.css";
+// import userpool from "./userpool";
+
+// function App() {
+//   const { isLogin, loading } = useAuth();
+
+//   if (loading) return <div>Loading...</div>;
+//   if (!isLogin) return <div>Not logged in</div>;
+
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/signup" element={<Signup />} />
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/dashboard" element={<Dashboard />} />
+//         <Route path="/billOverview" element={<BillOverview />} />
+//         <Route path="/businessCostSplit" element={<BusinessCostSplit />} />
+//         <Route path="/inventoryCostSplit" element={<InventoryCostSplit />} />
+//         <Route path="/orphanedSnapshots" element={<OrphanedSnapshots />} />
+//         <Route
+//           path="/unattachedManagedDisks"
+//           element={<UnattachedManagedDisks />}
+//         />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
@@ -9,20 +53,15 @@ import BusinessCostSplit from "./components/BusinessCostSplit";
 import InventoryCostSplit from "./components/InventoryCostSplit";
 import OrphanedSnapshots from "./components/OrphanedSnapshots";
 import UnattachedManagedDisks from "./components/UnattachedManagedDisks";
-import HyperScalarAdvisor from "./components/HyperScalarAdvisor";
-import SqlVmLicenses from "./components/SqlVmLicenses";
-import OrphanedRSVBackups from "./components/OrphanedRSVBackups";
+import { useAuth } from "./components/useAuth";
 
 import "./App.css";
-import userpool from "./userpool";
 
 function App() {
-  useEffect(() => {
-    let user = userpool.getCurrentUser();
-    if (user) {
-      // <Navigate to="/dashboard" replace />
-    }
-  }, []);
+  const { isLogin, loading, roles } = useAuth();
+
+  if (loading) return <div>Loading...</div>;
+  if (!isLogin) return <div>Not logged in</div>;
 
   return (
     <BrowserRouter>
@@ -30,6 +69,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/billOverview" element={<BillOverview />} />
         <Route path="/businessCostSplit" element={<BusinessCostSplit />} />
@@ -39,9 +79,6 @@ function App() {
           path="/unattachedManagedDisks"
           element={<UnattachedManagedDisks />}
         />
-        <Route path="/hyperScalarAdvisor" element={<HyperScalarAdvisor />} />
-        <Route path="/sqlVmLicenses" element={<SqlVmLicenses />} />
-        <Route path="/orphanedrsvbackups" element={<OrphanedRSVBackups />} />
       </Routes>
     </BrowserRouter>
   );
