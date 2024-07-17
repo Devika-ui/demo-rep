0d110252c77681332e7be542e727ea2a5e954096
Sai Greeshma <sai.greeshmakoyyada@dxc.com>
2024-07-17 19:55:39 +0530

    modified azure button

--- src/components/HeaderButtons.js
+++ src/components/HeaderButtons.js
@@ -3,63 +3,118 @@ import '../css/HeaderButtons.scss';
 import Azurelogo from '../images/Azure-Logo-PNG-Images.png';
 import AWSlogo from '../images/aws-logo.png';
 // import AWSlogo from '../images/Amazon_Web_Services-Logo.png';
-//import NewAWSlogo from '../images/icons8-amazon-web-services-32.png';
- import NewAWSlogo from '../images/azure-logo-svg.svg';
-
+// import NewAWSlogo from '../images/icons8-amazon-web-services-32.png';
+import NewAWSlogo from '../images/azure-logo-svg.svg';
 
 const HeaderButtons = ({ onButtonClick }) => {
+  const [previousState, setPreviousState] = useState(null);
+  const [currentState, setCurrentState] = useState(null);
+
   const handleClick = (value) => {
-    onButtonClick(value);
-  };
-
-  const [buttonStates, setButtonStates] = useState({
-    cut: false,
-    copy: false,
-    paste: false,
-  });
-
-  const generateElements = (type, count) => {
-    const elements = [];
-
-    for (let i = 0; i < count; i++) {
-      const element = document.createElement('SPAN');
-      element.classList.add('fas');
-      element.classList.add(`fa-${type}`);
-      element.classList.add(type);
-
-      elements.push(element);
+    if (currentState === value) {
+      // Reverting to previous state
+      setCurrentState(previousState);
+      onButtonClick(previousState);
+    } else {
+      // Saving the current state and setting the new state
+      setPreviousState(currentState);
+      setCurrentState(value);
+      onButtonClick(value);
     }
-
-    return elements;
   };
 
-  const animateElements = (elements) => {
-    elements.forEach((element) => {
-      document.body.appendChild(element);
-      element.classList.add('animated');
-    });
-
-    setTimeout(() => {
-      elements.forEach((element) => {
-        element.classList.remove('animated');
-        element.remove();
-      });
-    }, 3500);
-  };
-
-  const handleButtonClick = (type) => {
-    const count = 15; // Number of elements to generate
-    const elements = generateElements(type, count);
-
-    // Disable the button to prevent repeated clicks
-    setButtonStates((prevStates) => ({
-      ...prevStates,
-      [type]: true,
-    }));
-
-    // Add animation class and create elements
-    animateElements(elements);
-  };
+  // const HeaderButtons = ({ onButtonClick }) => {
+  //   const handleClick = (value) => {
+  //     onButtonClick(value);
+  //   };
+  
+  //   const [buttonStates, setButtonStates] = useState({
+  //     cut: false,
+  //     copy: false,
+  //     paste: false,
+  //   });
+  
+  //   const generateElements = (type, count) => {
+  //     const elements = [];
+  
+  //     for (let i = 0; i < count; i++) {
+  //       const element = document.createElement('SPAN');
+  //       element.classList.add('fas');
+  //       element.classList.add(fa-${type});
+  //       element.classList.add(type);
+  
+  //       elements.push(element);
+  //     }
+  
+  //     return elements;
+  //   };
+  
+  //   const animateElements = (elements) => {
+  //     elements.forEach((element) => {
+  //       document.body.appendChild(element);
+  //       element.classList.add('animated');
+  //     });
+  
+  //     setTimeout(() => {
+  //       elements.forEach((element) => {
+  //         element.classList.remove('animated');
+  //         element.remove();
+  //       });
+  //     }, 3500);
+  //   };
+  
+  //   const handleButtonClick = (type) => {
+  //     const count = 15; // Number of elements to generate
+  //     const elements = generateElements(type, count);
+  
+  //     // Disable the button to prevent repeated clicks
+  //     setButtonStates((prevStates) => ({
+  //       ...prevStates,
+  //       [type]: true,
+  //     }));
+  
+  //     // Add animation class and create elements
+  //     animateElements(elements);
+  //   };
+  
+  //   return (
+  //     <div className="button-container"> {/* Wrapper container for buttons */}
+  //       <div className="button-box">
+  //         <button
+  //           id="cut-btn"
+  //           onClick={() => handleClick('Azure')}
+  //           disabled={buttonStates.cut}
+  //           className="logo-button"
+  //           style={{ width: '36px', height: '36px' }} // Decrease button size
+  //         >
+  //           {/* <img src={Azurelogo} alt="Azure Logo" /> */}
+  //           <img src={NewAWSlogo} alt="Azure Logo" />
+  //         </button>
+  //         {/* <div className="separator"></div> {/* Add the separator */}
+  //         {/* <button
+  //           id="copy-btn"
+  //           onClick={() => handleButtonClick('CommonView')}
+  //           disabled={buttonStates.copy}
+  //           className="icon-button"
+  //           style={{ width: '30px', height: '30px' }} // Decrease button size
+  //         >
+  //           <i className="fas fa-copy"></i>
+  //         </button> */} 
+  //         <div className="separator"></div> {/* Add the separator */}
+  //         <button
+  //           id="paste-btn"
+  //           onClick={() => handleClick('AWS')}
+  //           disabled={buttonStates.paste}
+  //           className="logo-button"
+  //           style={{ width: '36px', height: '36px' }} // Decrease button size
+  //         >
+  //           <img src={AWSlogo} alt="AWS Logo" />
+  //         </button>
+  //       </div>
+  //     </div>
+  //   );
+  // };
+  
 
   return (
     <div className="button-container"> {/* Wrapper container for buttons */}
@@ -67,28 +122,15 @@ const HeaderButtons = ({ onButtonClick }) => {
         <button
           id="cut-btn"
           onClick={() => handleClick('Azure')}
-          disabled={buttonStates.cut}
           className="logo-button"
           style={{ width: '36px', height: '36px' }} // Decrease button size
         >
-          {/* <img src={Azurelogo} alt="Azure Logo" /> */}
           <img src={NewAWSlogo} alt="Azure Logo" />
         </button>
-        {/* <div className="separator"></div> {/* Add the separator */}
-        {/* <button
-          id="copy-btn"
-          onClick={() => handleButtonClick('CommonView')}
-          disabled={buttonStates.copy}
-          className="icon-button"
-          style={{ width: '30px', height: '30px' }} // Decrease button size
-        >
-          <i className="fas fa-copy"></i>
-        </button> */} 
         <div className="separator"></div> {/* Add the separator */}
         <button
           id="paste-btn"
           onClick={() => handleClick('AWS')}
-          disabled={buttonStates.paste}
           className="logo-button"
           style={{ width: '36px', height: '36px' }} // Decrease button size
         >
